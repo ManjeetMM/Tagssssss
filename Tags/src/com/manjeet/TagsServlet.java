1fecd6dd5e852af7c13ed4e1477fae01b31a5cc0
ManjeetMurari <manjeet.murari@a-cti.com>
2015-01-09 21:29:04 +0530

    First Tag

---
+++ Tags/src/com/manjeet/TagsServlet.java
@@ -0,0 +1,13 @@
+package com.manjeet;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TagsServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
